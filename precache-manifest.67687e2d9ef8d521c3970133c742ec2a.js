0e4af4b6d26658390d4c1919173c0ebd67c4bfb0
crystalYY <972369976@qq.com>
2019-07-22 10:17:29 +0800

    Updates

---
+++ precache-manifest.67687e2d9ef8d521c3970133c742ec2a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52f0a1a2709eca14aca99db9d57f4eda",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "349c44ba58c482c4dbec",
+    "url": "/todolist/static/css/main.01c70f46.chunk.css"
+  },
+  {
+    "revision": "58ad173cd721ecc045af",
+    "url": "/todolist/static/js/2.44361bd4.chunk.js"
+  },
+  {
+    "revision": "349c44ba58c482c4dbec",
+    "url": "/todolist/static/js/main.3af6dbc6.chunk.js"
+  },
+  {
+    "revision": "5225356577f332e6df46",
+    "url": "/todolist/static/js/runtime~main.d3bf418b.js"
+  }
+]);
